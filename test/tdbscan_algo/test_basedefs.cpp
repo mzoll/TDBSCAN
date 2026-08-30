396b48f8eb4a89154aa781473cb35a0a39a4ef7e
mzoll <marcel.zoll.pysics@gmail.com>
2026-08-30 13:58:27 +0200

    get tests underway

--- test/tdbscan_algo/test_basedefs.cpp
+++ test/tdbscan_algo/test_basedefs.cpp
@@ -4,10 +4,36 @@
 
 #include <gtest/gtest.h>
 
+#include "common_defs.h"
+
+
 // Demonstrate some basic assertions.
-TEST(OtherName, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+TEST(CommonDefs, TimeAdheres) {
+  EXPECT_EQ(double(MyTime_t(42)), 42);
+
+  EXPECT_EQ(MyTime_t(42.), MyTime_t(42.) );
+
+  EXPECT_TRUE(MyTime_t::min() < MyTime_t::max() );
+
+  EXPECT_EQ(MyTime_t(42.)- MyTime_t(42.),0. );
+  EXPECT_EQ(MyTime_t(42.)- MyTime_t(0.),42. );
 }
+
+TEST(CommonDefs, Position3dAdheres) {
+  EXPECT_EQ(Position3d(42,42,42), Position3d(42,42,42));
+
+  EXPECT_EQ(Position3d(0,0,0).magnitude(), 0);
+  EXPECT_EQ(Position3d(1,1,1).magnitude(), sqrt(3) );
+
+  EXPECT_EQ(Position3d(0,0,0).distance(Position3d(1,1,1)), sqrt(3) );
+
+  EXPECT_TRUE(Position3d(0,0,0) == Position3d(0,0,0));
+  EXPECT_TRUE(Position3d(1,2,3) == Position3d(1,2,3));
+  EXPECT_FALSE(Position3d(42,2,1) == Position3d(1,2,3));
+
+  EXPECT_FALSE(Position3d(0,0,0) < Position3d(0,0,0));
+  EXPECT_TRUE(Position3d(0,0,0) < Position3d(1,0,0));
+  EXPECT_FALSE(Position3d(1,0,0) < Position3d(0,0,0));
+}
+
+// Demonstrate some basic assertions.
