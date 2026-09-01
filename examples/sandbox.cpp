be525224642005f0aeb198d067c137b19e92f182
mzoll <marcel.zoll.pysics@gmail.com>
2026-09-01 14:11:16 +0200

    try code-formatting

--- examples/sandbox.cpp
+++ examples/sandbox.cpp
@@ -12,25 +12,23 @@ using namespace std;
 
 struct inverse_int {
   int value_;
+
   bool operator<(const inverse_int rhs) const {
     return value_ > rhs.value_;
   }
-  inverse_int(int a) : value_(a) {};
+
+  explicit inverse_int(const int a) : value_(a) {};
 };
 
 
-
 int main() {
-
-
-
   std::set<ScalarTime_t> s;
   s.insert(ScalarTime_t(1));
   s.insert(ScalarTime_t(9));
   s.insert(ScalarTime_t(5));
 
-  cout << "abc "<< endl;
-  for (const auto& a : s)
+  cout << "abc " << endl;
+  for (const auto &a: s)
     cout << a << endl;
 
 
