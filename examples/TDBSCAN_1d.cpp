b6a9ae889d7f3eb8d54a9989a1d0b782a0ce636f
mzoll <marcel.zoll.pysics@gmail.com>
2026-09-25 09:39:44 +0200

    make the box move continiously

--- examples/TDBSCAN_1d.cpp
+++ examples/TDBSCAN_1d.cpp
@@ -1,8 +1,13 @@
+//
+// Created by mzoll on 01/08/2026.
+//
+
 #include "tdbscan_algo/tdbscan_algo.h"
 
 #include <cstdlib>
 #include <format>
 #include <exception>
+#include <cmath>
 #include <fmt/format.h>
 
 #include "tdbscan_algo/common_defs.h"
@@ -13,8 +18,8 @@
 using namespace std;
 using namespace tdbscan;
 
-// define time as a pure positive unit
 
+// create a random double on
 double rand_double() {
 	double lower_bound = 0.;
 	double upper_bound = 1.;
@@ -35,6 +40,7 @@ public:
 		SIGNAL =100,
 	} origin_{UNKNOWN};
 
+  ///convenience to convert `Origin` into a string for human reading
   inline
   static std::string origin_tostr(const Origin o) {
     switch (o) {
@@ -66,6 +72,7 @@ public:
   std::ostream& operator<< ( std::ostream& outs, const SBlibWithTrace & st);
 };
 
+/// custom ostream output; nicely formats the class as a string
 inline
 std::ostream& operator<< ( std::ostream& os, const SBlibWithTrace & sblib_trace) {
   return os <<
@@ -73,7 +80,7 @@ std::ostream& operator<< ( std::ostream& os, const SBlibWithTrace & sblib_trace)
       static_cast<ScalarBlib>(sblib_trace);
 };
 
-
+/// custom formatter for SBlibWithTrace. Used in conjunction with `std::format` or `fmt::format`
 template <>
 struct std::formatter<SBlibWithTrace> {
   constexpr auto parse(std::format_parse_context & ctx) {return ctx.begin();}
@@ -87,24 +94,33 @@ struct std::formatter<SBlibWithTrace> {
 };
 
 
-
-
-// define some Limiters
+// ===================== Define some Limiters ====================
+// We create two simple limiters: One to limit distance and one to Limit time between Blibs
+// Both of them are Connectors and compare one Blib to another.
+// The only thing we need to implement is the the `bool eval(Blib rhs, Blib rhs)´ function.
+// In the end we combine both Limiters into a single `ConnectorBlock`, which than can be used in the TDBscan-algorithm
+// ========================================================
 class DistanceLimiter final : public ConnectorSingle<SBlibWithTrace> {
 public:
 	SBlibWithTrace::Ordinate_t::Distance_t maxDist_;
-	DistanceLimiter(const SBlibWithTrace::Ordinate_t::Distance_t maxDistance) : ConnectorSingle("DistConnector"), maxDist_(maxDistance) {};
+	DistanceLimiter(const SBlibWithTrace::Ordinate_t::Distance_t maxDistance) :
+  ConnectorSingle("DistConnector"), maxDist_(maxDistance) {};
 
-	bool eval(const SBlibWithTrace& lhs, const SBlibWithTrace& rhs) const {return abs(lhs.distanceTo(rhs)) <= maxDist_;};
+	bool eval(const SBlibWithTrace& lhs, const SBlibWithTrace& rhs) const {
+	  return abs(lhs.distanceTo(rhs)) <= maxDist_;
+	};
 };
 
 // make one connector which just connects to max time-diff; this is time ordered and thereby is positive one-sided
 class TimeLimiter final : public ConnectorSingle<SBlibWithTrace> {
 public:
 	SBlibWithTrace::Time_t::TimeDiff_t maxTimediff_;
-	explicit TimeLimiter(const SBlibWithTrace::Time_t::TimeDiff_t maxTimeDiff) : ConnectorSingle("DistConnector"), maxTimediff_(maxTimeDiff) {};
+	explicit TimeLimiter(const SBlibWithTrace::Time_t::TimeDiff_t maxTimeDiff) :
+  ConnectorSingle("DistConnector"), maxTimediff_(maxTimeDiff) {};
 
-	bool eval(const SBlibWithTrace& lhs, const SBlibWithTrace& rhs) const {return rhs.timeTo(lhs) <= maxTimediff_;};
+	bool eval(const SBlibWithTrace& lhs, const SBlibWithTrace& rhs) const {
+	  return rhs.timeTo(lhs) <= maxTimediff_;
+	};
 };
 
 // combine the Connectors into a ConnectorBlock
@@ -113,8 +129,10 @@ class LimitingConnector final : public ConnectorBlock<SBlibWithTrace> {
 
 
 
-// ========================= PARTS for the main algo =========================
-
+/* ============================ Constructing the TDBscan algorithm instance ==================
+ * the main algorithm is constructed by creating instances of the limiters defined in the previous set,
+ * and providing a set of Algorithm parameters
+ */
 TDBScan_Algo<SBlibWithTrace> construct_algo(const double distance_lim, const double time_lim) {
 
 	auto distLimiter_ = new DistanceLimiter(distance_lim);
@@ -126,16 +144,22 @@ TDBScan_Algo<SBlibWithTrace> construct_algo(const double distance_lim, const dou
 	TDBScan_Algo<SBlibWithTrace>::TDBScan_ParameterSet params;
 
 	params.multiplicity=4;
-	params.multiplicityTimeWindow=2;
-	params.emergenceTimeWindow=2;
-	params.earlyMergeOverlapRatio= 1.;
-	params.lateMergeOverlapRatio= 1.;
+	params.multiplicityTimeWindow=2.; //internally converted into SBlibWithTrace<..., tTime>::tTime::Time_t
+	params.emergenceTimeWindow=2.;//internally converted into SBlibWithTrace<..., tTime>::tTime::Time_t
+	params.earlyMergeOverlapRatio= 1.; //ZeroOne: its a ratio
+	params.lateMergeOverlapRatio= 1.; //ZeroOne: its a ratio
 
 	return TDBScan_Algo<SBlibWithTrace>(params, limcon);
 }
 
 
-
+/* ========================= Create the scenario =======================
+ * For a demonstration and testing scenario Blibs need to be generated which stem from two distinguishable sources:
+ * Signal and Noise.
+ * In Our case of a 1d- grid, the signal will be a represented by a box of finite size moving sideways
+ * with a given inertia generating blibs according to a brightness. Noise on the other hand will be random noise
+ * uniformly generated on the fields of that grid.
+ */
 std::set<SBlibWithTrace>
 generate_noise(const double noise_freq, const double width_fields, const double time_duration) {
 	std::set<SBlibWithTrace> blibs;
@@ -160,25 +184,44 @@ generate_noise(const double noise_freq, const double width_fields, const double 
  * --.------[ xx ]------------
  * --.--------[  xx]----------
  *
+ * The box will be reflected on the right and left side
+ *
  * @param box_size
  * @param inerta
- * @param ledge_start_pos
+ * @param start_pos,
  * @param brightness a measure of the signal frequency in one unit-volume of the box
+ * @param field_size,
  * @param time_duration
  * @return
  */
 std::set<SBlibWithTrace>
-generate_moving_box(const double box_size, const double inerta, const double ledge_start_pos, const double brightness, const double time_duration) {
+generate_moving_box(
+  const double box_size,
+  const double inertia,
+  const double start_pos,
+  const double brightness,
+  const double field_size,
+  const double time_duration) {
 	std::set<SBlibWithTrace> blibs;
 
-	for (int time_step = 0; time_step < time_duration; time_step++) {
-		const auto box_ledge_pos = time_step * inerta + ledge_start_pos;
-	  for (int j = 0; j < brightness * box_size; j++) {
-			const auto t = time_step + rand_double();
-			const auto pos = box_ledge_pos + rand_double() * box_size ;
-			blibs.insert(SBlibWithTrace({pos}, t, SBlibWithTrace::SIGNAL));
-		}
-	}
+  const int n_blibs = static_cast<int>(time_duration * box_size * brightness);
+
+  for (int i_blib = 0; i_blib < n_blibs; i_blib++) {
+    const auto t = rand_double() * time_duration;
+    const auto center_pos_ind = (t*inertia + start_pos) / field_size;
+
+    const double center_pos = field_size * (int(center_pos_ind) % 2 == 0 ?  center_pos_ind - std::floor(center_pos_ind) : 1. - (center_pos_ind - std::floor(center_pos_ind)));
+
+    const double blib_pos_inbox = rand_double() * box_size - box_size/2.;
+
+    const auto blib_pos = center_pos + blib_pos_inbox;
+
+    if (blib_pos<0. || blib_pos > field_size)
+      continue;
+
+    blibs.insert(SBlibWithTrace({blib_pos}, t, SBlibWithTrace::SIGNAL));
+  }
+
 	return blibs;
 }
 
@@ -194,10 +237,10 @@ generate_moving_box(const double box_size, const double inerta, const double led
  * @return
  */
 std::set<SBlibWithTrace>
-gernerate_blibs( const double time_duration=50, const int width_fields = 100, const int brightness= 1, const double noise_contamination = 0.1) {
+gernerate_blibs( const double time_duration=50, const double width_fields = 100, const double brightness= 1., const double noise_contamination = 0.1) {
 	std::set<SBlibWithTrace> blibs;
 
-	const auto _box_blibs = generate_moving_box( 5, 1, 0, brightness, time_duration);
+	const auto _box_blibs = generate_moving_box(5., 1., 0., brightness, width_fields, time_duration);
 	LOG_INFO("Generated {} BOX blibs", _box_blibs.size());
   blibs.insert(_box_blibs.cbegin(), _box_blibs.cend());
 
@@ -208,8 +251,12 @@ gernerate_blibs( const double time_duration=50, const int width_fields = 100, co
 }
 
 
-/// calculate the purity, Signal over Noise ratio, of this Blib sample
-double calculate_signal_purity(const std::set<SBlibWithTrace> blibs) {
+/**
+ * Helper function: calculate the purity, Signal over Noise ratio, of this Blib sample
+ * @param blibs the blibs to quantify
+ * @return the purity as '#signal / #all'.
+ */
+double calculate_signal_purity(const std::set<SBlibWithTrace>& blibs) {
   int _signal_count = 0;
   int _noise_count = 0;
   for (const auto& b: blibs) {
@@ -233,7 +280,7 @@ int main(int argc, char **argv) {
 	auto my_algo = construct_algo(2., 0.5);
 
 	LOG_INFO("Generate blibs");
-	const auto blibs = gernerate_blibs(50 ,100, 3, 0.3 );
+	const auto blibs = gernerate_blibs(50 ,100, 3, 0.1 );
 
   LOG_INFO("Processing nBlibs: {} (purity {:.3f})", blibs.size(), calculate_signal_purity(blibs));
   //take first 3
